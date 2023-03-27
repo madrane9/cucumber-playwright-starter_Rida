f6bbcda4a7d35f210a071e587684f7ec9d9ad07d
madrane9 <madrane9@hotmail.com>
2023-03-27 12:18:31 +0200

    Verschieben des allgemeingültigen Schritts

--- src/steps/general-api.steps.ts
+++ src/steps/general-api.steps.ts
@@ -1,26 +1,8 @@
 import { ICustomWorld } from '../support/custom-world';
-import { Given, Then, When } from '@cucumber/cucumber';
-import { expect } from '@playwright/test';
-
-Given('ein Request an die Katzen-Fakten-API', async function (this: ICustomWorld) {
-  this.requestPath = '/facts';
-});
-
-When('ich als limit Parameter {int} setze', async function (this: ICustomWorld, value: number) {
-  this.requestParams = { limit: value };
-});
+import { When } from '@cucumber/cucumber';
 
 When('ich den Request absende', async function (this: ICustomWorld) {
   this.requestResponse = await this.requestContext?.get(this.requestPath!, {
     data: this.requestParams,
   });
 });
-
-Then(
-  'erwarte ich {int} Fakten über Katzen',
-  async function (this: ICustomWorld, expectedResultCount: number) {
-    const buffer = await this.requestResponse?.body();
-    const result = JSON.parse(buffer!.toString());
-    expect(result.data.length).toEqual(expectedResultCount);
-  },
-);
