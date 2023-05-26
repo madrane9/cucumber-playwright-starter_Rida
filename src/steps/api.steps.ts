e5fd94eb5e74883edb4397610bf262ad6c774364
madrane9 <madrane9@hotmail.com>
2023-05-26 13:35:42 +0200

    pas++

--- src/steps/api.steps.ts
+++ src/steps/api.steps.ts
@@ -4,7 +4,7 @@ import { Given } from '@cucumber/cucumber';
 
 Given('ich möchte mich authetifizieren', async function (this: ICustomWorld) {
   await this.page?.goto('https://github.com/login');
-  await this.loginPage?.login('madrane9@hotmail.com', 'MA99ri71');
+  await this.loginPage?.login('madrane9@hotmail.com', '++++');
   // Wait until the page reaches a state where all cookies are set.
   await this.page?.waitForLoadState('load');
   // Save authentication state.
